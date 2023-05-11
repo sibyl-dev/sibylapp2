af3fb8bc9a205407bb18a6dd46f7731327a51da9
zyteka <zyteka@mit.edu>
2023-05-11 11:46:04 -0400

    Adding readme and cleaning configs

--- sibylapp/config.py
+++ sibylapp/config.py
@@ -1,11 +1,15 @@
-BAR_LENGTH = 8
-MAX_ENTITIES = 10
-
+# API CONFIGURATIONS ===================================================================================================
 BASE_URL = "http://localhost:3000/api/v1/"
 
+# APPLICATION-SPECIFIC CONFIGURATIONS ==================================================================================
 FLIP_COLORS = True  # If true, positive contributions will be red
 
 
-def pred_format_func(pred):
+def pred_format_func(pred):  # Function to use to format the prediction values coming out of your model
     return "${:,.2f}".format(pred)
     # return "failure" if pred else "normal"
+
+
+# OTHER CONFIGURATIONS =================================================================================================
+BAR_LENGTH = 8  # Number of squares to use for contribution/importance bars
+MAX_ENTITIES = 10  # Maximum number of entities to select from. Set this to None to use all entities.
