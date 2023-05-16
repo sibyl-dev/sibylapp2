124f55d78caf6a3abc3d4d1f833f66d53db46b5d
zyteka <zyteka@mit.edu>
2023-05-16 13:33:19 -0400

    Adding pre-written format options

--- sibylapp/config.py
+++ sibylapp/config.py
@@ -7,10 +7,27 @@ FLIP_COLORS = True  # If true, positive contributions will be red
 
 
 def pred_format_func(pred):  # Function to use to format the prediction values from model
-    return "${:,.2f}".format(pred)
-    # return "failure" if pred else "normal"
+    return dollar_format(pred)  # See pre-written options below for defaults
 
 
 # OTHER CONFIGURATIONS ============================================================================
 BAR_LENGTH = 8  # Number of squares to use for contribution/importance bars
 MAX_ENTITIES = 10  # Maximum number of entities to select from. Set this to None to use all
+
+
+# PRE-WRITTEN FORMAT FUNCTION OPTIONS =============================================================
+def no_format(pred):
+    return pred
+
+
+def dollar_format(pred):
+    return "${:,.2f}".format(pred)
+
+
+def class_name_format(pred, class_names):
+    return class_names[int(pred)]
+
+
+def boolean_name_format(pred, pos_name, neg_name):
+    return pos_name if pred else neg_name
+
