99cfce207eb839d5f69e63bae054267fc3a2487e
zyteka <zyteka@mit.edu>
2023-06-01 10:35:14 -0400

    Fixing bug with importance failing if contributions didn't finish

--- pages/2_Understand_the_Model.py
+++ pages/2_Understand_the_Model.py
@@ -1,9 +1,13 @@
 import streamlit as st
-from sibylapp import importance, filtering, explore_feature
+from sibylapp import importance, filtering, explore_feature, entities, config
 from sibylapp.context import get_term
 
 st.set_page_config(layout="wide")
 st.title("Sibyl")
+
+# Prepping data -----------------------------
+if "eids" not in st.session_state:
+    st.session_state["eids"] = entities.get_eids(max_entities=config.MAX_ENTITIES)
 
 
 # Prepping explanations ---------------------
