9ffc9132bf97545752ecc58bc266df03f38744e0
wei-enwang <warrenwang6303@gmail.com>
2023-09-20 18:13:45 -0400

    debug for no row_ids

--- pages/3_Compare_Cases.py
+++ pages/3_Compare_Cases.py
@@ -28,9 +28,17 @@ if tab == "1":
     filtering.view_entity_select(add_prediction=False)
     eid = st.session_state["eid"]
     row_ids = st.session_state["row_id_dict"][eid]
-    filtering.view_time_select(eid, row_ids, row_id_text="row_id", prefix="first", default=0)
-    filtering.view_time_select(eid, row_ids, row_id_text="row_id_comp", prefix="second", default=1)
-    entity_difference.view_instructions(use_row_ids=True)
+    if len(row_ids) < 2:
+        st.subheader(
+            "This entity contains one or zero rows of data. Difference within this entity not"
+            " well-defined."
+        )
+    else:
+        filtering.view_time_select(eid, row_ids, row_id_text="row_id", prefix="first", default=0)
+        filtering.view_time_select(
+            eid, row_ids, row_id_text="row_id_comp", prefix="second", default=1
+        )
+        entity_difference.view_instructions(use_row_ids=True)
 elif tab == "2":
     filtering.view_entity_select(eid_text="eid", prefix="first", default=0)
     filtering.view_entity_select(eid_text="eid_comp", prefix="second", default=1)
@@ -39,23 +47,24 @@ elif tab == "2":
 # Global options ------------------------------
 filtering.view_filtering()
 if tab == "1":
-    if st.session_state["row_id"] == st.session_state["row_id_comp"]:
-        st.subheader("Please select two different prediction times!")
-    else:
-        entity_difference.view_prediction_difference(
-            st.session_state["row_id"],
-            st.session_state["row_id_comp"],
-            use_row_ids=True,
-            row_ids=row_ids,
-            eid_for_rows=st.session_state["eid"],
-        )
-        entity_difference.view(
-            st.session_state["row_id"],
-            st.session_state["row_id_comp"],
-            use_row_ids=True,
-            row_ids=row_ids,
-            eid_for_rows=st.session_state["eid"],
-        )
+    if len(row_ids) >= 2:
+        if st.session_state["row_id"] == st.session_state["row_id_comp"]:
+            st.subheader("Please select two different prediction times!")
+        else:
+            entity_difference.view_prediction_difference(
+                st.session_state["row_id"],
+                st.session_state["row_id_comp"],
+                use_row_ids=True,
+                row_ids=row_ids,
+                eid_for_rows=st.session_state["eid"],
+            )
+            entity_difference.view(
+                st.session_state["row_id"],
+                st.session_state["row_id_comp"],
+                use_row_ids=True,
+                row_ids=row_ids,
+                eid_for_rows=st.session_state["eid"],
+            )
 elif tab == "2":
     if st.session_state["eid"] == st.session_state["eid_comp"]:
         st.subheader(
