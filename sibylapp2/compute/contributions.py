18397f1b0023d345344a9b3841e5a4c6a477177d
zyteka <zyteka@mit.edu>
2023-12-04 10:19:22 -0500

    Bugfix

--- sibylapp2/compute/contributions.py
+++ sibylapp2/compute/contributions.py
@@ -23,7 +23,7 @@ def get_dataset_contributions(model_id=api.fetch_model_id(), all_rows=True):
         all_rows and "dataset_row_id_dict" not in st.session_state
     ):
         st.session_state["dataset_eids"], st.session_state["dataset_row_id_dict"] = (
-            entities.get_eids(max_entities=1000, return_row_ids=all_rows)
+            entities.get_eids(max_entities=get_dataset_size(), return_row_ids=all_rows)
         )
     # confirm at least one entity has more than one row
     if any(len(lst) > 1 for lst in st.session_state["dataset_row_id_dict"].values()):
