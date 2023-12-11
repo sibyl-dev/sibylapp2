9fea1ca21c132f2804dc84672877c798d78ae1db
zyteka <zyteka@mit.edu>
2023-12-11 12:05:37 -0500

    Updating to remove changes when changing pages

--- pages/4_Experiment_with_Changes.py
+++ pages/4_Experiment_with_Changes.py
@@ -27,15 +27,11 @@ else:
         eid,
     )
 
-# Update displayed table after user presses the button
-if st.button("Run model and explanations on customized data"):
-    st.session_state["changes_to_show"] = copy.deepcopy(changes)
-
-if "changes_to_show" in st.session_state:
+if len(changes) > 0:
     if "use_rows" in st.session_state and st.session_state["use_rows"]:
         customized_entity.view_prediction(
             st.session_state["row_id"],
-            st.session_state["changes_to_show"],
+            changes,
             st.session_state["model_id"],
             use_row_id=True,
             eid_for_rows=eid,
@@ -43,7 +39,7 @@ if "changes_to_show" in st.session_state:
         filtering.view_filtering()
         customized_entity.view(
             st.session_state["eid"],
-            st.session_state["changes_to_show"],
+            changes,
             st.session_state["model_id"],
             st.session_state["row_id"],
         )
@@ -51,13 +47,15 @@ if "changes_to_show" in st.session_state:
     else:
         customized_entity.view_prediction(
             st.session_state["eid"],
-            st.session_state["changes_to_show"],
+            changes,
             st.session_state["model_id"],
         )
         filtering.view_filtering()
         customized_entity.view(
             st.session_state["eid"],
-            st.session_state["changes_to_show"],
+            changes,
             st.session_state["model_id"],
             None,
         )
+else:
+    st.info("Select features to change to see modified explanation")
