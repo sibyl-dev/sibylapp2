a97fedcadfeac6edfc8f1ae75ef140eb264a8abe
Ola Zytek <zyteka@mit.edu>
2024-01-10 13:16:32 -0500

    Updating names

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ from sibylapp2.pages import (
     Settings,
 )
 from sibylapp2.view.utils import setup
+from sibylapp2.compute.context import get_term
 
 setup.setup_page(return_row_ids=True)
 
 pages = {
     "Explore a Prediction": Explore_a_Prediction,
-    "Similar Cases": Similar_Cases,
-    "Compare Cases": Compare_Cases,
+    "Similar %s" % get_term("Entity", plural=True): Similar_Cases,
+    "Compare %s" % get_term("Entity", plural=True): Compare_Cases,
     "Experiment with Changes": Experiment_with_Changes,
     "Understand the Model": Understand_the_Model,
     "Settings": Settings,
 }
 page_select = st.sidebar.radio("Select an explanation", pages.keys())
+st.sidebar.divider()
 
 pages[page_select].main()
