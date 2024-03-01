bf10e2ce9690f60dff495df94cdb610e51d3f572
zyteka <zyteka@mit.edu>
2024-03-01 13:18:24 -0500

    Updating change over time page to support new structure

--- sibylapp2/pages/Change_over_Time.py
+++ sibylapp2/pages/Change_over_Time.py
@@ -5,21 +5,21 @@ import streamlit as st
 from sibylapp2.view import temporal_change
 from sibylapp2.view.utils import display, filtering, setup
 
-setup.setup_page(return_row_ids=True)
 
-# Sidebar ------------------------------------
-display.show_probability_select_box()
-filtering.view_selection()
-temporal_change.view_instructions()
-eid = st.session_state["eid"]
-row_ids = st.session_state["row_id_dict"][eid]
-model_ids = st.session_state["model_ids"]
+def main():
+    # Sidebar ------------------------------------
+    display.show_probability_select_box()
+    filtering.view_selection()
+    temporal_change.view_instructions()
+    eid = st.session_state["eid"]
+    row_ids = st.session_state["row_id_dict"][eid]
+    model_ids = st.session_state["model_ids"]
 
-# Global options ------------------------------
-if len(row_ids) == 1 and len(model_ids) < 1:
-    st.warning(
-        "This page is only supported for datasets with multiple observations per entity and"
-        " multiple models."
-    )
-else:
-    temporal_change.view(eid, st.session_state["row_id"], model_ids=model_ids)
+    # Global options ------------------------------
+    if len(row_ids) == 1 and len(model_ids) < 1:
+        st.warning(
+            "This page is only supported for datasets with multiple observations per entity and"
+            " multiple models."
+        )
+    else:
+        temporal_change.view(eid, st.session_state["row_id"], model_ids=model_ids)
